bed305fb7782fd9a20daf7d20ff39a1e39ebc5c6
Juliet Brown <juliet@silverstripe.com>
2017-10-16 23:07:49 +1300

    added gify api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import {Grid, Jumbotron} from 'react-bootstrap';
 import SearchForm from './components/SearchForm';
-import Results from './components/Results';
+import GifList from './components/GifList';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Jumbotron>
-            <Grid>
-              <h1>Search app</h1>
-              <p>This is a simple search app</p>
-              <SearchForm />
-            </Grid>
-        </Jumbotron>
-        <Results />
-      </div>
+
+    constructor() {
+        super();
+        this.state = {
+            gifs: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+            .then(response => response.json())
+            .then(responseData => {
+                this.setState({ gifs: responseData.data });
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error);
+            });
+    }
+
+    // performSearch = () {
+
+    // }
+
+    render() {
+        console.log(this.state.gifs);
+        return (
+          <div className="App">
+            <Jumbotron>
+                <Grid>
+                  <h1>Search app</h1>
+                  <p>This is a simple search app</p>
+                  <SearchForm />
+                </Grid>
+            </Jumbotron>
+            <GifList data={this.state.gifs} />
+          </div>
     );
   }
 }
--- my-app/src/components/SearchForm.js
+++ my-app/src/components/SearchForm.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import 	{ Form, FormGroup, FormControl, Button} from 'react-bootstrap'
+import React, { Component } from 'react';
 
-const SearchForm = () => (
-	<Form inline>
-	  <FormGroup controlId="formInlineEmail">
-	    <FormControl type="search" placeholder="enter something..." />
-	  </FormGroup>
-	  {' '}
-	  <Button type="submit">
-	    search
-	  </Button>
-	</Form>
-);
-
-export default SearchForm;
+export default class SearchForm extends Component {
+  
+  state = {
+    searchText: ''
+  }
+  
+  onSearchChange = e => {
+    this.setState({ searchText: e.target.value });
+  }
+  
+  handleSubmit = e => {
+    e.preventDefault();
+    e.currentTarget.reset();
+  }
+  
+  render() {  
+    return (
+      <form className="search-form" onSubmit={this.handleSubmit} >
+        <label className="is-hidden" htmlFor="search">Search</label>
+        <input type="search" 
+               onChange={this.onSearchChange}
+               name="search" 
+               placeholder="Search..." />
+        <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
+      </form>      
+    );
+  }
+}
