7a3262d46c4fd273d160029da7feb092eb529561
Juliet Brown <juliet@silverstripe.com>
2017-10-24 10:04:25 +1300

    working with redux implemented

--- react-gif-search/src/index.js
+++ react-gif-search/src/index.js
@@ -1,64 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/SearchBar';
-import GifList from './components/GifList';
-import request from 'superagent';
-import GifModal from './components/GifModal';
-import './styles/app.css';
+import App from './containers/App';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-class App extends React.Component {
-
-	constructor() {
-        super();
-
-        this.state = {
-            gifs: [],
-            selectedGif: null,
-            modalIsOpen: false
-        }
-
-        this.handleTermChange = this.handleTermChange.bind(this);
-    }
-
-    openModal(gif) {
-        this.setState({
-            modalIsOpen: true,
-            selectedGif: gif
-        });
-    }
-
-    closeModal() {
-        this.setState({
-            modalIsOpen: false,
-            selectedGif: null
-        });
-    }
-
-	handleTermChange(term) {
-    	const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
-
-        request.get(url, (err, res) => {
-            this.setState({ gifs: res.body.data })
-        });
-  	}
-
-    render() {
-        return (
-            <div>
-                <SearchBar onTermChange={term => this.handleTermChange(term)}/>
-                <GifList  gifs={this.state.gifs}
-                 onGifSelect={selectedGif => this.openModal(selectedGif) } />
-                <GifList gifs={this.state.gifs} />
-                <GifModal modalIsOpen={this.state.modalIsOpen}
-                          selectedGif={this.state.selectedGif}
-                          onRequestClose={ () => this.closeModal() } />
-            </div>
-        );
-    }
-}
+const store = configureStore();
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('body')
 );
  
